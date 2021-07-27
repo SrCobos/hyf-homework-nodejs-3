0923e0052b5924dd11d14f40b22f9870a23db99b
SrCobos <63576338+SrCobos@users.noreply.github.com>
2021-07-27 13:02:52 -0600

    Update index.js
    Se adjunto la ruta users

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+    res.send('Haz recibido al usuario')
+})
+
 app.listen(3000, function() { 
     console.log( "The server is running" ) ;
 })
