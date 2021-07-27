4dc50b4873ce066e66b9522ad48e02e2028d373d
SrCobos <63576338+SrCobos@users.noreply.github.com>
2021-07-27 12:58:04 -0600

    Update index.js
    Se agrego el Endpoint users

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+    res.send('El usuario ha sido recibido !!!')
+})
+
 app.listen(3000, function() { 
     console.log( "The server is running" ) ;
 })
