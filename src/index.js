8771983cf9aa48fd62a94a9ed35803e012196972
SrCobos <63576338+SrCobos@users.noreply.github.com>
2021-07-27 13:05:41 -0600

    Update index.js
    New update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require ('express');
 const app = express();
 
+const  mockUserData = [ 
+	{ nombre : 'Mark' } , 
+	{ nombre : 'Jill' } 
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/users', (req, res) => {
-    res.send('Haz recibido al usuario')
+app.get('/users', function (req, res) { 
+	res.json({ 
+		success : true, 
+		message :'consiguió usuarios con éxito. ¡ Bien !', 
+		users : mockUserData
+	}) 
 })
 
 app.listen(3000, function() { 
